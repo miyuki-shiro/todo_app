ce3f911884c79e546c26a6f697680330406ae30b
miyuki-shiro <thaimyg@yahoo.cl>
2021-01-04 12:44:58 -0300

    First commit

---
+++ test_driver/app_test.dart
@@ -0,0 +1,85 @@
+// Imports the Flutter Driver API.
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Todo App', () {
+    // First, define the Finders and use them to locate widgets from the
+    // test suite. Note: the Strings provided to the `byValueKey` method must
+    // be the same as the Strings we used for the Keys in step 1.
+
+    // login screen
+    final usernameField = find.byValueKey('username');
+    final passwordField = find.byValueKey('password');
+    final signInButton = find.byValueKey('signIn');
+    final createAccountButton = find.byValueKey('createAccount');
+
+    // home screen
+    final signOutButton = find.byValueKey('signOut');
+    final addField = find.byValueKey('addField');
+    final addButton = find.byValueKey('addButton');
+
+    FlutterDriver driver;
+
+    Future<bool> isPresent(SerializableFinder byValueKey, {Duration timeout = const Duration(seconds: 1)}) async {
+      try {
+        await driver.waitFor(byValueKey, timeout: timeout);
+        return true;
+      } catch (exception) {
+        return false;
+      }
+    }
+
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    // testing createAccount()
+    test('create account', () async {
+      if (await isPresent(signOutButton)) {
+        await driver.tap(signOutButton);
+      }
+
+      await driver.tap(usernameField);
+      await driver.enterText("tgm@example.com");
+
+      await driver.tap(passwordField);
+      await driver.enterText("123456");
+
+      await driver.tap(createAccountButton);
+      await driver.waitFor(find.text("Your Todos"));
+    });
+
+    // testing login()
+    test('login', () async {
+      if (await isPresent(signOutButton)) {
+        await driver.tap(signOutButton);
+      }
+
+      await driver.tap(usernameField);
+      await driver.enterText("tgm@example.com");
+
+      await driver.tap(passwordField);
+      await driver.enterText("123456");
+
+      await driver.tap(signInButton);
+      await driver.waitFor(find.text("Your Todos"));
+    });
+
+    // testing addATodo()
+    test('add a todo', () async {
+      await driver.tap(addField);
+      await driver.enterText("te amo <3");
+      await driver.tap(addButton);
+      await driver.waitFor(find.text("te amo <3"), timeout: const Duration(seconds: 5));
+    });
+  });
+}
